d230ce982308dc3aa310f690338a66c06537a780
Wilmata24 <wilmermata.24@gmail.com>
2020-06-28 15:25:41 -0600

    Matgrid

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,
+            MatGridListModule]
 })
 export class AppRoutingModule { }
